3fa6d9e486d3c745a064fcfc3390a404d2f579ff
James Reeves <jreeves@weavejester.com>
2013-01-26 14:29:14 +0000

    Added codox plugin for API documentation

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-codec "1.6"]]
+  :plugins [[codox "0.6.4"]]
+  :codox {:src-dir-uri "http://github.com/ring-clojure/ring-codec/blob/master"
+          :src-linenum-anchor-prefix "L"}
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}})
