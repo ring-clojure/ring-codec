2b24260c616f011749b399927c0805535bae8007
James Reeves <jreeves@weavejester.com>
2014-05-12 21:51:21 +0100

    Updated Codox to 0.8.0

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-codec "1.6"]]
-  :plugins [[codox "0.6.4"]]
-  :codox {:src-dir-uri "http://github.com/ring-clojure/ring-codec/blob/1.0.0"
+  :plugins [[codox "0.8.0"]]
+  :codox {:src-dir-uri "http://github.com/ring-clojure/ring-codec/blob/1.0.0/"
           :src-linenum-anchor-prefix "L"}
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
