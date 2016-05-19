6ada05a68717235c8a9fb5b2df45fa62103394e2
James Reeves <jreeves@weavejester.com>
2016-05-19 19:39:12 +0100

    Update Codox to 0.9.5

--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-codec "1.6"]]
-  :plugins [[codox "0.8.0"]]
-  :codox {:src-dir-uri "http://github.com/ring-clojure/ring-codec/blob/1.0.0/"
-          :src-linenum-anchor-prefix "L"}
+  :plugins [[lein-codox "0.9.5"]]
+  :codox
+  {:output-path "codox"
+   :source-uri "http://github.com/ring-clojure/ring-codec/blob/{version}/{filepath}#L{line}"}
   :aliases {"test-all" ["with-profile" "default:+1.4:+1.5:+1.6:+1.7:+1.8" "test"]}
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
