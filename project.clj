d4580df3e4ad1b50ea87b473ae5db6e06494ee00
James Reeves <jreeves@weavejester.com>
2021-02-16 23:41:47 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [commons-codec "1.11"]]
-  :plugins [[lein-codox "0.10.3"]]
+                 [commons-codec "1.15"]]
+  :plugins [[lein-codox "0.10.7"]]
   :codox
   {:output-path "codox"
    :source-uri "http://github.com/ring-clojure/ring-codec/blob/{version}/{filepath}#L{line}"}
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
   :profiles
-  {:dev  {:dependencies [[criterium "0.4.4"]]}
+  {:dev  {:dependencies [[criterium "0.4.6"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
