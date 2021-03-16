e907ee8b614371bf2f688a4f65d31c857cd313ea
Harold <hhausman@gmail.com>
2021-03-16 08:27:23 -0600

    Add type hint to silence reflection warnings

--- src/ring/util/codec.clj
+++ src/ring/util/codec.clj
@@ -77,7 +77,7 @@
 
 (defn base64-encode
   "Encode an array of bytes into a base64 encoded string."
-  [unencoded]
+  [^bytes unencoded]
   (String. (.encode (Base64/getEncoder) unencoded)))
 
 (defn base64-decode
